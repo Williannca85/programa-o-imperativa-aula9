0d6f6b00878e3ce1fbb811b87c4cb6102fe69f37
Williannca85 <87399058+Williannca85@users.noreply.github.com>
2022-03-11 21:23:50 -0300

    Update microondas.js

--- microondas.js
+++ microondas.js
@@ -1,4 +1,82 @@
-function rapidFood(opcao, tempo) {
+function rapidFood(prato, tempo) {
+switch(prato) {
+  case 1:
+      if (tempo >= 30){
+          console.log('Kabumm')
+      }
+      else if (tempo >= 20 ){
+          console.log('Comida queimou')
+      }
+      else if (tempo < 10) {
+          console.log('Tempo insuficiente')
+      }
+      else {
+          console.log('Prato pronto, bom apetite!')
+      }
+      break;
+  case 2:
+      if (tempo >= 24){
+          console.log('Kabumm')
+      }
+      else if (tempo >= 16 ){
+          console.log('Comida queimou')
+      }
+      else if (tempo < 8) {
+          console.log('Tempo insuficiente')
+      }
+      else {
+          console.log('Prato pronto, bom apetite!')
+      }
+      break;
+  case 3:
+      if (tempo >= 45){
+          console.log('Kabumm')
+      }
+      else if (tempo >= 30 ){
+          console.log('Comida queimou')
+      }
+      else if (tempo < 15) {
+          console.log('Tempo insuficiente')
+      }
+      else {
+          console.log('Prato pronto, bom apetite!')
+      }
+      break;
+  case 4:
+      if (tempo >= 36){
+          console.log('Kabumm')
+      }
+      else if (tempo >= 24 ){
+          console.log('Comida queimou')
+      }
+      else if (tempo < 12) {
+          console.log('Tempo insuficiente')
+      }
+      else {
+          console.log('Prato pronto, bom apetite!')
+      }
+      break;
+  case 5:
+      if (tempo >= 24){
+          console.log('Kabumm')
+      }
+      else if (tempo >= 16 ){
+          console.log('Comida queimou')
+      }
+      else if (tempo < 8) {
+          console.log('Tempo insuficiente')
+      }
+      else {
+          console.log('Prato pronto, bom apetite!')
+      }
+      break;
+}
+}
+
+console.log(rapidFood(5,13));
+
+
+/*function rapidFood(opcao, tempo) {
   let msg;
 
 if (opcao > 5) {
@@ -48,6 +126,7 @@ if (opcao > 5) {
 }
 return msg;
 }
+*/
 
 /*console.log("1: Pipoca");
 console.log("2: Macarrão");
@@ -55,5 +134,5 @@ console.log("3: Carne");
 console.log("4: Feijão");
 console.log("5: Brigadeiro");*/
 
-console.log(rapidFood(1, 20));
+//console.log(rapidFood(1, 20));
 
